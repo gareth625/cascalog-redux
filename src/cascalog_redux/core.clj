8b4fb9a5af42c7060eaab769e6f59b3c703b3c94
gareth625 <gareth.j.rogers@gmail.com>
2015-02-07 16:50:37 +0000

    Added in paths to the data files in core.clj for convenience. Also added a little doc to -main.

--- src/cascalog_redux/core.clj
+++ src/cascalog_redux/core.clj
@@ -3,8 +3,14 @@
             [cascalog-redux.taps :as bike-trip-data])
   (:gen-class))
 
+(def bike-trip-data-sample
+  "data/bike_trip_data_sample.csv")
+
+(def bike-trip-data-full
+  "data/bike_trip_data.csv")
+
 (defn -main
-  ""
+  "Streams the input bike trip data file to stdout. You probably don't want to stream the whole file ;)"
   [input & args]
   (let [bad-rows-sink (hfs-textline "br")]
     (?- (bike-trip-data/out-sink "stdout")
